7b452ecdf94c99a84b1daab8cd32f8c086c967ce
Casper <casper@ckubo.com>
2020-10-01 20:28:59 +0200

    ...

--- ChromaticMIDI.ino
+++ ChromaticMIDI.ino
@@ -21,19 +21,60 @@ void controlChange(byte channel, byte control, byte value){
 //=============================================================================================
 // SETUP
 //=============================================================================================
+
+// LAYOUT:
+// usb *top*
+// r1
+// r2
+//
+//
+// r3 --- btns
+// r4 --- slider
+// r5 --- c8
+// r6 --- c7
+// r7 --- c6
+// r8 --- c5
+// c1 --- c4
+// c2 --- c3
+
+//#define NUM_ROWS 8 //inputs
 #define NUM_ROWS 2 //inputs
 #define NUM_COLS 8 //outputs
 
-const int rowPin[NUM_ROWS] = {10, 16};
-const int colPin[NUM_COLS] = {2, 3, 4, 5, 6, 7, 8, 9};
+const int rowPin[NUM_ROWS] = {6, 7};
+const int colPin[NUM_COLS] = {8, 9, 10, 16, 14, 15, 18, 19};
 
 int rowVal[NUM_ROWS];
 
 unsigned long lastDebounceTime[NUM_ROWS][NUM_COLS] = {0};
-unsigned long debounceDelay = 5;
+unsigned long debounceDelay = 2;
 
-int note = 60; //lowest note
+const int btnsPin = A3;
+int btnsCounter = 0;
+long btnsTime = 0;
+int btnsDebounceCount = 5;
+int btnsCurrentState = 0;
+int btnsVal;
+
+const int potPin = A2;
+byte potMode = 0;
+int potCState = 0;
+int potPState = 0;
+int potVar = 0;
+int potMidiCState = 0;
+int potMidiPState = 0;
+int potVelCState = 0;
+int potVelPState = 0;
+int potTimeOut = 300;
+int potVarThreshold = 10;
+boolean potMoving = true;
+unsigned long potPTime = 0;
+unsigned long potTimer = 0;
+
+int note = 43; //lowest note
+byte velocity = 63;
 byte midiCh = 0;
+byte midiCC = 1;
 
 boolean keyPressed[NUM_ROWS][NUM_COLS];
 uint8_t keyToMidiMap[NUM_ROWS][NUM_COLS];
@@ -57,6 +98,8 @@ void setup() {
     }
   }
 
+  pinMode(btnsPin, INPUT_PULLUP);
+
 }
 
 //=============================================================================================
@@ -65,6 +108,10 @@ void setup() {
 void loop() {
 
   keys();
+
+  pots();
+
+  btns();
 
 }
 
@@ -90,7 +137,7 @@ void keys() {
             lastDebounceTime[row][col] = millis();
   
             keyPressed[row][col] = true;
-            noteOn(midiCh, keyToMidiMap[row][col], 63);
+            noteOn(midiCh, keyToMidiMap[row][col], velocity);
             MidiUSB.flush();
           }
         }
@@ -100,11 +147,101 @@ void keys() {
       for (int row = 0; row < NUM_ROWS; ++row) {
         if (rowVal[row] == HIGH && keyPressed[row][col]) {
           keyPressed[row][col] = false;
-          noteOff(midiCh, keyToMidiMap[row][col], 63);
+          noteOff(midiCh, keyToMidiMap[row][col], velocity);
           MidiUSB.flush();
         }
       }     
       
     }
     
+}
+
+//=============================================================================================
+// POTS
+//=============================================================================================
+void pots() {
+  potCState = analogRead(potPin);
+  potMidiCState = map(potCState, 0, 1023, 127, 0);
+  potVelCState = map(potCState, 0, 1023, 127, 0);
+
+  potVar = abs(potCState - potPState);
+  if (potVar > potVarThreshold){
+    potPTime = millis();
+  }
+  potTimer = millis() - potPTime;
+  if (potTimer < potTimeOut){
+    potMoving = true;
+  } else {
+    potMoving = false;
+  }
+
+  if (potMode == 0 && potMoving == true && potMidiPState != potMidiCState){ 
+    controlChange(midiCh, midiCC + 0, potMidiCState);
+    MidiUSB.flush();
+
+    potPState = potCState;
+    potMidiPState = potMidiCState;
+  }
+
+  if (potMode == 1 && potMoving == true && potVelPState != potVelCState){
+    velocity =  potVelCState;
+
+    potPState = potCState;
+    potVelPState = potVelCState;
+  }
+  
+}
+
+//=============================================================================================
+// BTNS
+//=============================================================================================
+void btns() {
+//  Serial.println(analogRead(btnsPin));
+//  delay(100);
+  if (millis() != btnsTime) {
+    btnsVal = analogRead(btnsPin);
+    if (btnsVal == btnsCurrentState && btnsCounter > 0) {
+      btnsCounter--;
+    }
+    if (btnsVal != btnsCurrentState) {
+      btnsCounter++;
+    }
+    if (btnsCounter >= btnsDebounceCount) {
+      btnsCounter = 0;
+      btnsCurrentState = btnsVal;
+      if (btnsVal > 0) {
+        // L: 73 - 76
+        // R: 41 - 44
+        // U: 52 - 56 || 36 - 39
+        // D: 19 - 23
+        if (btnsVal >= 73 && btnsVal <= 76) {
+          potMode = 0;
+        }
+        if (btnsVal >= 41 && btnsVal <= 44) {
+          potMode = 1;
+        }
+        if ((btnsVal >= 52 && btnsVal <= 56) || (btnsVal >= 36 && btnsVal <= 39)) {
+          changeOctave(true);
+        }
+        if (btnsVal >= 19 && btnsVal <= 23) {
+          changeOctave(false);
+        }
+        
+      }
+    }
+    btnsTime = millis();
+  }
+
+}
+
+void changeOctave(bool up) {  
+  for(int row = 0; row < NUM_ROWS; ++row){
+    for (int col = 0; col < NUM_COLS; ++col) {
+      if (up) {
+        keyToMidiMap[row][col] = keyToMidiMap[row][col] + 12; 
+      } else {
+        keyToMidiMap[row][col] = keyToMidiMap[row][col] - 12; 
+      }
+    }
+  }
 }
--- README.md
+++ README.md
@@ -11,11 +11,22 @@ _(schematics based off Dmitry Yegorenkov's [AccordionMega project](https://githu
 | Opto-interrupter (GP1S094HCZ0F) | 58     |
 | Transistor (BC547B)             | 8      |
 | Resistor 330Ω                   | 58     |
-| Resistor 1kΩ                    | 8      |
+| Resistor 1kΩ                    | 9      |
+| Resistor 2kΩ                    | 1      |
+| Resistor 4kΩ                    | 1      |
+| Resistor 230Ω                   | 1      |
+| Tumbler switch                  | 2      |
+| Potentiometer linear 10kΩ       | 1      |
+| Usb c female                    | 2      |
+| Usb micro b male                | 1      |
 |                                 |        |
 | Protoboard 40 x 2.5 cm          | 1      |
+| Protoboard 4 x 1.8 cm           | 1      |
 | Female headers 2 x 8            | 16     |
-| Female headers 2 x 1            | 1      |
+| Female headers 1 x 12           | 2      |
+| Female headers 1 x 2            | 2      |
 | Male headers 2 x 2              | 58     |
+| Male headers 2 x 8              | 1      |
 | Male headers 1 x 12             | 2      |
+| Male headers 1 x 2              | 2      |
 | Ribbon cable, wire              | /      |
